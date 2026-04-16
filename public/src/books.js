0078623571165e6d1aab992859bd0d49963e6ffd
Michael <unioncollegemichael@yahoo.com>
2026-04-16 18:35:03 -0400

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,30 +1,28 @@
-function findAuthorById(authors, id) {
-  return authors.find((author) => author.id === id);
-}
+const findAuthorById = (authors, id) => authors.find((author) => author.id === id);
 
-function findBookById(books, id) {
-  return books.find((book) => book.id === id);
-}
+const findBookById = (books, id) => books.find((book) => book.id === id);
 
-function partitionBooksByBorrowedStatus(books) {
-  const borrowed = [];
-  const returned = [];
+const partitionBooksByBorrowedStatus = (books) => {
+  const borrowedBooks = [];
+  const returnedBooks = [];
 
   books.forEach((book) => {
-    const latestBorrow = book.borrows[0];
-    if (latestBorrow && latestBorrow.returned === false) borrowed.push(book);
-    else returned.push(book);
+    if (book.borrows[0].returned) {
+      returnedBooks.push(book);
+    } else {
+      borrowedBooks.push(book);
+    }
   });
 
-  return [borrowed, returned];
-}
+  return [borrowedBooks, returnedBooks];
+};
 
-function getBorrowersForBook(book, accounts) {
+const getBorrowersForBook = (book, accounts) => {
   return book.borrows.slice(0, 10).map((borrow) => {
-    const account = accounts.find((acc) => acc.id === borrow.id);
+    const account = accounts.find((account) => account.id === borrow.id);
     return { ...account, returned: borrow.returned };
   });
-}
+};
 
 module.exports = {
   findAuthorById,
