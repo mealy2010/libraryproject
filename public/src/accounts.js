c4d7b73de271b8df6a72d0d0d500cb711a7c0520
Michael <unioncollegemichael@yahoo.com>
2026-04-16 18:34:33 -0400

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,45 +1,26 @@
-function findAccountById(accounts, id) {
-  return accounts.find((account) => account.id === id);
-}
+const getTotalNumberOfBorrows = (account, books) => {
+  return books.reduce((totalBorrows, book) => {
+    const borrowCount = book.borrows.filter(
+      (borrow) => borrow.id === account.id
+    ).length;
 
-function sortAccountsByLastName(accounts) {
-  return accounts.sort((a, b) =>
-    a.name.last.toLowerCase().localeCompare(b.name.last.toLowerCase())
-  );
-}
-
-function getAccountFullNames(accounts) {
-  return accounts.map((account) => `${account.name.first} ${account.name.last}`);
-}
-
-function getTotalNumberOfBorrows(account, books) {
-  return books.reduce((total, book) => {
-    const count = book.borrows.filter((borrow) => borrow.id === account.id).length;
-    return total + count;
+    return totalBorrows + borrowCount;
   }, 0);
-}
+};
 
-function getBooksPossessedByAccount(account, books, authors) {
-  const checkedOut = books.filter((book) => {
-    const latest = book.borrows[0];
-    return latest && latest.id === account.id && latest.returned === false;
+const getBooksPossessedByAccount = (account, books, authors) => {
+  const checkedOutBooks = books.filter((book) => {
+    const currentBorrow = book.borrows[0];
+    return currentBorrow.id === account.id && !currentBorrow.returned;
   });
 
-  return checkedOut.map((book) => {
-    const author = authors.find((a) => a.id === book.authorId);
+  return checkedOutBooks.map((book) => {
+    const author = authors.find((author) => author.id === book.authorId);
     return { ...book, author };
   });
-}
+};
 
-/**
- * IMPORTANT:
- * Export each function explicitly to avoid “overwritten module.exports” issues.
- * (This is the most bulletproof way to satisfy the test import.)
- */
 module.exports = {
-  findAccountById,
-  sortAccountsByLastName,
-  getAccountFullNames,
   getTotalNumberOfBorrows,
   getBooksPossessedByAccount,
 };
